90297e76f09b114d590fdf8365fb4d72d892c725
Mathieu <77482141+Mathieeeu@users.noreply.github.com>
2024-09-04 11:06:43 +0200

    slider + style

--- Polyrithms/script.js
+++ Polyrithms/script.js
@@ -1,14 +1,21 @@
 class Circle {
-    constructor(rayon,x,y,angle) {
+    constructor(rayon,x,y,angle,color) {
         this.rayon = rayon;
         this.x = x;
         this.y = y;
         this.angle = 0;
+        this.color = color;
     }
-    draw(ctx,color) {
+    draw(ctx) {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.rayon, 0, 2 * Math.PI);
-        ctx.strokeStyle = color;
+        ctx.strokeStyle = this.color;
+        if (this.color == color1) {
+            ctx.lineWidth = 3;
+        }
+        else {
+            ctx.lineWidth = 5;
+        }
         ctx.stroke();
     }
 }
@@ -33,19 +40,21 @@ function sleep(ms) {
 async function drawCircles() {
     let radius = minRadius;
     for (let i = 0; i < nbCircles; i++) {
-        tabCircles[i] = new Circle(radius, x_center, y_center,0);
-        tabCircles[i].draw(ctx,color);
+        tabCircles[i] = new Circle(radius, x_center, y_center,0,color1);
+        tabCircles[i].draw(ctx);
         await sleep(100-2*i);
         tabPoints[i] = new Point(x_center + radius, y_center);
         tabPoints[i].draw(ctx,color);
         radius += (canvas.height - minRadius) / (2.5*nbCircles);
     }
     circlesReady = true;
+    document.getElementById('start').style.visibility = 'visible';
 }
 
 function start() {
     if (circlesReady) {
-        document.getElementById('start').style.display = 'none';
+        document.getElementById('start').style.visibility = 'hidden';
+        document.getElementById('slidecontainer').style.visibility = 'visible'; 
         animate(tabCircles);
     }
 }
@@ -56,12 +65,17 @@ function playSound(url) {
 }
 
 function animate(tabCircles) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas once per frame
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < nbCircles; i++) {
-        tabCircles[i].angle += vitesse / tabCircles[i].rayon;
+        tabCircles[i].angle += speed.value/1000 / tabCircles[i].rayon;
         if (tabCircles[i].angle > 2 * Math.PI) {
-            console.log('sounds/'+ instrument + '/' + i%nbSounds + '.mp3');
+            // console.log('circle:' + i + ' - sounds/' + instrument + '/' + i%nbSounds + '.mp3');
             playSound('sounds/'+ instrument + '/' + i%nbSounds + '.mp3');
+
+            tabCircles[i].color = color2;
+            setTimeout(() => {
+                tabCircles[i].color = color;
+            }, 100);
         }
         let x = tabCircles[i].rayon * Math.cos(tabCircles[i].angle) + x_center;
         let y = tabCircles[i].rayon * Math.sin(tabCircles[i].angle) + y_center;
@@ -70,7 +84,7 @@ function animate(tabCircles) {
             tabCircles[i].angle = 0;
         }
         point.draw(ctx,color);
-        tabCircles[i].draw(ctx,color);
+        tabCircles[i].draw(ctx);
     }
     requestAnimationFrame(() => animate(tabCircles));
 }
@@ -85,15 +99,21 @@ const nbCircles = 27;
 let minRadius = 25;
 let tabCircles = [];
 let tabPoints = [];
-let color = '#f2f4d1';
+let color1 = '#f2f4d1';
 let color2 = '#179204';
+let color = color1;
 
 const instrument = 'vibraphone';
 const nbSounds = 10;
 
-const vitesse = 2;
 let framerate = 60;
 let interval = 1000 / framerate;
 
-drawCircles();
+let speed = document.getElementById("speed");
+let output = document.getElementById("value");
+output.innerHTML = speed.value/1000;
+speed.oninput = function() {
+    output.innerHTML = this.value/1000;
+}
 
+drawCircles();
