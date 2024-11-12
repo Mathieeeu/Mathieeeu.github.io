a8c770d47cb70a38f64aeb2cead09d8d8b8d91c8
Mathieu <77482141+Mathieeeu@users.noreply.github.com>
2024-11-13 00:01:31 +0100

    Update box-redirect.js

--- box-redirect.js
+++ box-redirect.js
@@ -1,3 +1,6 @@
 document.getElementById('polyrithms-box').addEventListener('click', function() {
     window.location.href = 'Polyrithms/index.html';
 });
+document.getElementById('cours').addEventListener('click', function() {
+    window.location.href = 'cours/index.html';
+});
